d392c50fee00acddd05a6f6395b320970814508c
rinnothing <maxrizhalex@gmail.com>
2023-09-22 10:34:21 +0300

    docs: add docstring to all the functions inside circle.py, rectangle.py, square.py, triangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,26 @@
 def area(a, b):
+    """
+    Calculates the area of a rectangle given the sides lengths.
+
+        Parameters:
+            a (int): The length of a side of the rectangle
+            b (int): The length of a side of the rectangle
+
+        Returns:
+            area (int): The area of the rectangle with a and b sides
+    """
     return a*b
 
+
 def perimeter(a, b):
+    """
+    Calculates the perimeter of a rectangle given the sides lengths.
+
+        Parameters:
+            a (int): The length of a side of a side of the rectangle
+            b (int): The length of a side of a side of the rectangle
+
+        Returns:
+            perimeter (int): The perimeter of the rectangle with a and b sides
+    """
     return (a+b)*2
--- triangle.py
+++ triangle.py
@@ -1,5 +1,27 @@
-def area(a, h): 
-    return a * h / 2 
+def area(a, h):
+    """
+    Calculates the area of a triangle given the length of a side and the height to the side.
 
-def perimeter(a, b, c): 
-    return a + b + c 
+        Parameters:
+            a (int): The length of a side of the triangle
+            h (int): The height to the side of a triangle
+
+        Returns:
+            area (int): The area of the triangle with a side and h height
+    """
+    return a * h / 2
+
+
+def perimeter(a, b, c):
+    """
+    Calculates the perimeter of a triangle given the sides lengths.
+
+        Parameters:
+            a (int): The length of a side of the triangle
+            b (int): The length of a side of the triangle
+            c (int): The length of a side of the triangle
+
+        Returns:
+            perimeter (int): The perimeter of the triangle with a, b and c sides
+    """
+    return a + b + c
