76dbb78b0f0eacbd5a7cd3b1b3f1db93e52a2076
dinary-upos <max.orel.site@yandex.kz>
2023-09-22 12:04:30 +0300

    L-05: Add comments to code

--- circle.py
+++ circle.py
@@ -1,10 +1,18 @@
 import math
 
 
+'''
+area(r: float): float
+this function calculates circle area by it's radius
+'''
 def area(r):
     return math.pi * r * r
 
 
+'''
+perimeter(r: float): float
+this function calculates circumference length by radius of circle
+'''
 def perimeter(r):
     return 2 * math.pi * r
 
--- triangle.py
+++ triangle.py
@@ -1,4 +1,14 @@
+'''
+area(a: float, h: float): float
+this function calculates area of triangle by it's side (a) and
+height (h), lowered to this side
+'''
 def area(a, h):
     return a * h / 2
+
+'''
+perimeter(a: float, b: float, c: float): float
+this function calculates perimeter of triangle by it's three sides
+'''
 def perimeter(a, b, c):
     return a + b + c
