fff07baecfc369acbe99cb11b479fba131acb741
Ivan Kramarenko <ivankramarenko13@gmail.com>
2023-09-23 10:38:08 +0300

    docs: Add docstrings for functions in circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,25 @@ import math
 
 
 def area(r):
+    """Returns circle area with given radius.
+
+    :param r: circle radius
+    :type r: int|float
+
+    :rtype: float
+    :return: circle area
+    """
     return math.pi * r * r
 
 
 def perimeter(r):
+    """Returns circle perimetr with given radius
+
+    :param r: circle radius
+    :type r: int|float
+
+    :rtype: float
+    :return: circle perimeter
+    """
     return 2 * math.pi * r
 
