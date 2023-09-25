9230c35997afcc15bc2c734efab09effbc83ab28
yar4123 <belyaroslav@mail.ru>
2023-09-25 17:39:41 +0300

    feat: function tips

--- circle.py
+++ circle.py
@@ -2,9 +2,17 @@ import math
 
 
 def area(r):
+    '''
+        Принимает число r и возвращает площадь круга с радиусом r.
+        Пример: area(3) = pi * 9
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+        Принимает число r и возвращает длину окружности с радиусом r.
+        Пример: perimeter(3) = pi * 6
+    '''
     return 2 * math.pi * r
 
