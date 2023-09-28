176089e305a7be8fa5cafdb50f26ff6d1e268605
AgA101 <91223978+AgA101@users.noreply.github.com>
2023-09-28 22:57:04 +0300

    Update rectangle.py
    Add: description of functions

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,18 @@
 def area(a, b):
+     '''
+    Calculate area for rectangle
+    
+    Parameters: a, b (int/float): sides
+    Return value: a * b (int/float): area
+    '''
     return a * b
 
 
 def perimeter(a, b):
+     '''
+    Calculate perimeter for rectangle
+    
+    Parameters: a, b (int/float): sides
+    Return value: (a + b) * 2 (int/float): perimeter
+    '''
     return (a + b) * 2
