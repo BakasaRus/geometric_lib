c33ecb587919a02114b0fe687b92e059ba86d39c
AgA101 <91223978+AgA101@users.noreply.github.com>
2023-09-28 23:17:32 +0300

    Update square.py
    Add: description of functions

--- square.py
+++ square.py
@@ -1,7 +1,19 @@
 
 def area(a):
+    '''
+    Calculates area for rectangle
+    
+    Parameters: a (int/float): side
+    Return value: a * a (int/float): area
+    '''
     return a * a
 
 
 def perimeter(a):
+    '''
+    Calculates perimeter for square
+    
+    Parameters: a (int/float): side
+    Return value: 4 * a (int/float): perimeter
+    '''
     return 4 * a
