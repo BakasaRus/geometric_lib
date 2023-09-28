054ca9bd12319e001477c2a18961243e9943fc33
AgA101 <91223978+AgA101@users.noreply.github.com>
2023-09-28 22:09:16 +0300

    Update circle.py
    Add description of functions

--- circle.py
+++ circle.py
@@ -2,9 +2,21 @@ import math
 
 
 def area(r):
+    '''
+    Calculate area for circle
+    
+    Parameters: r (int/float): radius
+    Return value: math.pi * r * r (int/float): area
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''
+    Calculate perimeter for circle
+    
+    Parameters: r (int/float): radius
+    Return value: math.pi * r * r (int/float): perimeters
+    '''
     return 2 * math.pi * r
 
