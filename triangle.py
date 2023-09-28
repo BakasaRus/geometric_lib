acbff82f9a1a8150ecba4659b8ca524e62e37569
AgA101 <91223978+AgA101@users.noreply.github.com>
2023-09-28 23:22:50 +0300

    Update triangle.py
    Add: description of functions

--- triangle.py
+++ triangle.py
@@ -1,5 +1,17 @@
 def area(a, h): 
+    '''
+    Calculate area for triangle
+    
+    Parameters: a, h (int/float): side and height
+    Return value: a * h / 2 (int/float): area
+    '''
     return a * h / 2 
 
-def perimeter(a, b, c): 
+def perimeter(a, b, c):
+    '''
+    Calculate perimeter for triangle
+    
+    Parameters: a, b, c (int/float): sides
+    Return value: a + b + c (int/float): perimeter
+    '''
     return a + b + c 
