05b924400ca8f9352b91bbc5d12451e0e59d7df9
Ladoshkina Natalya <ladoshkinan@mail.ru>
2023-09-30 19:16:54 +0300

    added description of functions in circle.py, rectangle.py, square.py, triangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,24 @@
 def area(a, b):
+    '''
+        Calculates the area of a rectangle with sides a and b
+            Parameters:
+                a (int/float): first side of the rectamgle
+                b (int/float): second side of the rectamgle
+                
+            Returns:
+                area (int/float): the area of the rectangle with sides a and b
+    '''
     return a * b
 
 def perimeter(a, b):
+    '''
+        Calculates the perimeter of a rectangle with sides a and b
+            Parameters:
+                a (int/float): first side of the rectamgle
+                b (int/float): second side of the rectamgle
+
+            Returns:
+                perimeter (int/float): the perimeter of the rectangle with sides a and b
+    '''
     return 2 * (a + b)
 
--- triangle.py
+++ triangle.py
@@ -1,5 +1,24 @@
 def area(a, h):
+    '''
+        Calculates the area of a triangle with side a and height h
+            Parameters:
+                a (int/float): side of the triangle
+                h (int/float): height of the triangle
+
+            Returns:
+                area (int/float): the area of a triangle with side a and height h
+    '''
     return a * h / 2
 
 def perimeter(a, b, c):
+    '''
+        Calculates the perimeter of a triangle with sides a, b and c
+            Parameters:
+                a (int/float): first side of the rectamgle
+                b (int/float): second side of the rectamgle
+                c (int/float): third side of the rectamgle
+
+            Returns:
+                perimeter (int/float): the perimeter of the triangle with sides a, b and c
+    '''
     return a + b + c
