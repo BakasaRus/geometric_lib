72942aff18d846ffa4902960802184a7497b8a73
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2023-10-01 12:52:36 +0300

    Added docstrings to functions

--- circle.py
+++ circle.py
@@ -1,10 +1,23 @@
 import math
+from typing import Union
 
 
-def area(r):
+def area(r: Union[int, float]) -> Union[int, float]:
+    """
+    Принимает радиус круга и возвращает площадь круга
+
+    :param r: радиус круга
+    :return: площадь круга
+    """
     return math.pi * r * r
 
 
-def perimeter(r):
+def perimeter(r: Union[int, float]) -> Union[int, float]:
+    """
+    Принимает радиус круга и возвращает площадь круга
+
+    :param r: радиус круга
+    :return: площадь круга
+    """
     return 2 * math.pi * r
 
--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,23 @@
-def area(a, b): 
-    return a * b 
+from typing import Union
 
-def perimeter(a, b): 
+
+def area(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    """
+    Принимает длины сторон прямоугольника и возвращает площадь прямоугольника
+
+    :param a: длина прямоугольника
+    :param b: ширина прямоугольника
+    :return: площадь прямоугольника со сторонами a и b
+    """
+    return a * b
+
+
+def perimeter(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    """
+    Принимает длины сторон прямоугольника и возвращает периметр прямоугольника
+
+    :param a: длина прямоугольника
+    :param b: ширина прямоугольника
+    :return: периметр прямоугольника со сторонами a и b
+    """
     return (a + b) * 2
