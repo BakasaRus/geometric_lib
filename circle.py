cc69822b4053d376be5ab2a95c2d62ef4f250e64
Lena <lena.dorohina@12gmail.com>
2023-10-16 02:04:00 +0300

    added function descriptions

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Принимает радиус круга r, возвращает площадь круга'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Принимает радиус круга r, возвращает периметр круга'''
     return 2 * math.pi * r
 
