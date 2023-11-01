be6c08a282063e249a64439cf938f4781456f0ef
biqiboqi <409701@niuitmo.ru>
2023-11-02 00:37:35 +0300

    [test] Added test for lib

--- circle.py
+++ circle.py
@@ -1,5 +1,5 @@
 import math
-
+import unittest
 def area(r):
     '''
     Возвращает площадь круга радиуса r
@@ -11,7 +11,6 @@ def area(r):
             math.pi * r * r (float): площадь круга
     '''
     return math.pi * r * r
-
 
 def perimeter(r):
     '''
@@ -25,3 +24,60 @@ def perimeter(r):
     '''
     return 2 * math.pi * r
 
+
+class CircleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_one_area(self):
+        res = area(1)
+        self.assertEqual(res, 3.141592653589793)
+
+    def test_two_area(self):
+        res = area(2)
+        self.assertEqual(res, 12.566370614359172)
+
+    def test_half_area(self):
+        res = area(0.5)
+        self.assertEqual(res, 0.7853981633974483)
+
+    def test_third_area(self):
+        res = area(0.333)
+        self.assertEqual(res, 0.3483680677639186)
+
+    def test_big1_area(self):
+        res = area(12345)
+        self.assertEqual(res, 478775657.3542472)
+
+    def test_big2_area(self):
+        res = area(1020)
+        self.assertEqual(res, 3268512.9967948208)
+
+    def test_zero_perimeter(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_one_perimeter(self):
+        res = perimeter(1)
+        self.assertEqual(res, 6.283185307179586)
+
+    def test_two_perimeter(self):
+        res = perimeter(2)
+        self.assertEqual(res, 12.566370614359172)
+
+    def test_half_perimeter(self):
+        res = perimeter(0.5)
+        self.assertEqual(res, 3.141592653589793)
+
+    def test_third_perimeter(self):
+        res = perimeter(0.333)
+        self.assertEqual(res, 2.092300707290802)
+
+    def test_big1_perimeter(self):
+        res = perimeter(12345)
+        self.assertEqual(res, 77565.92261713199)
+
+    def test_big2_perimeter(self):
+        res = perimeter(1020)
+        self.assertEqual(res, 6408.849013323178)
--- rectangle.py
+++ rectangle.py
@@ -1,3 +1,4 @@
+import unittest
 def area(a, b):
     '''
     Возвращает площадь прямоугольника со сторонами a, b
@@ -24,3 +25,69 @@ def perimeter(a, b):
             2 * (a * b)  (float): периметр прямоугольника
     '''
     return 2 * (a + b)
+
+
+class RectangleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0, 1)
+        self.assertEqual(res, 0)
+        res = area(1, 0)
+        self.assertEqual(res, 0)
+        res = area(0, 0)
+        self.assertEqual(res, 0)
+
+    def test_one_area(self):
+        res = area(1, 1)
+        self.assertEqual(res, 1)
+
+    def test_two_square_area(self):
+        res = area(2, 2)
+        self.assertEqual(res, 4)
+
+    def test_half_area(self):
+        res = area(0.5, 0.5)
+        self.assertEqual(res, 0.25)
+
+    def test_third_area(self):
+        res = area(0.332, 3)
+        self.assertEqual(res, 0.996)
+
+    def test_big1_area(self):
+        res = area(12345, 2)
+        self.assertEqual(res, 24690)
+
+    def test_big2_area(self):
+        res = area(1020, 1020)
+        self.assertEqual(res, 1040400)
+
+    def test_zero_perimeter(self):
+        res = perimeter(0, 0)
+        self.assertEqual(res, 0)
+        res = perimeter(0 , 1)
+        self.assertEqual(res, 2)
+        res = perimeter(1, 0)
+        self.assertEqual(res, 2)
+
+    def test_one_perimeter(self):
+        res = perimeter(1, 1)
+        self.assertEqual(res, 4)
+
+    def test_two_perimeter(self):
+        res = perimeter(2, 1)
+        self.assertEqual(res, 6)
+
+    def test_half_perimeter(self):
+        res = perimeter(0.5, 0.5)
+        self.assertEqual(res, 2)
+
+    def test_third_perimeter(self):
+        res = perimeter(0.333, 1)
+        self.assertEqual(res, 2.666)
+
+    def test_big1_perimeter(self):
+        res = perimeter(12345, 1242)
+        self.assertEqual(res, 27174)
+
+    def test_big2_perimeter(self):
+        res = perimeter(1020, 1021)
+        self.assertEqual(res, 4082)
