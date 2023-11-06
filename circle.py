d84eba618ea0443cf9b820db971dad742fac013b
yakarych <kaarych@gmail.com>
2023-11-06 20:57:00 +0300

    Add unit-tests

--- circle.py
+++ circle.py
@@ -1,4 +1,5 @@
 import math
+import unittest
 
 
 def area(r: float) -> float:
@@ -18,3 +19,20 @@ def perimeter(r: float) -> float:
     """
     return 2 * math.pi * r
 
+
+class CircleTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_area(self):
+        res = area(10)
+        self.assertEqual(res, 100 * math.pi)
+
+    def test_zero_perimeter(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_perimeter(self):
+        res = perimeter(10)
+        self.assertEqual(res, 20 * math.pi)
--- square.py
+++ square.py
@@ -1,3 +1,5 @@
+import unittest
+
 
 def area(a: float) -> float:
     """
@@ -15,3 +17,21 @@ def perimeter(a: float) -> float:
     :returns perimeter
     """
     return 4 * a
+
+
+class SquareTestCase(unittest.TestCase):
+    def test_zero_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_area(self):
+        res = area(10)
+        self.assertEqual(res, 100)
+
+    def test_zero_perimeter(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_perimeter(self):
+        res = perimeter(10)
+        self.assertEqual(res, 40)
