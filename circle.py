50b069671d3f244aec933897281a05161a27e3e9
Engozdev <denacua5@gmail.com>
2023-11-12 17:13:41 +0300

    test: added test and test report

--- circle.py
+++ circle.py
@@ -1,12 +1,45 @@
-import math
+import math, unittest
 
 
 def area(r):
-    '''Принимает радиус круга, возвращает площадь круга переданного радиуса'''
+    """Принимает радиус круга, возвращает площадь круга переданного радиуса"""
     return math.pi * r * r
 
 
 def perimeter(r):
-    '''Принимает радиус круга, возвращает периметр круга переданного радиуса'''
+    """Принимает радиус круга, возвращает периметр круга переданного радиуса"""
     return 2 * math.pi * r
 
+
+class CircleTestCase(unittest.TestCase):
+    def test_zero_radius_area(self):
+        res = area(0)
+        self.assertEqual(res, 0)
+
+    def test_zero_radius_perimeter(self):
+        res = perimeter(0)
+        self.assertEqual(res, 0)
+
+    def test_small_radius_area(self):
+        res = area(3)
+        self.assertEqual(res, 28.274333882308138)
+
+    def test_small_perimeter_perimeter(self):
+        res = perimeter(3)
+        self.assertEqual(res, 18.84955592153876)
+
+    def test_big_radius_area(self):
+        res = area(100000)
+        self.assertEqual(res, 31415926535.89793)
+
+    def test_big_radius_perimeter(self):
+        res = perimeter(100000)
+        self.assertEqual(res, 628318.5307179586)
+
+    def test_negative_radius_area(self):
+        res = area(-2)
+        self.assertEqual(res, -1)
+
+    def test_negative_radius_perimeter(self):
+        res = perimeter(-2)
+        self.assertEqual(res, -1)
--- triangle.py
+++ triangle.py
@@ -1,7 +1,49 @@
-def area(a, h): 
-    '''Принимает длину стороны и высоты треугольника, возвращает площадь соответсвующего треугольника'''
-    return a * h / 2 
+import unittest
 
-def perimeter(a, b, c): 
-    '''Принимает длины сторон треугольника, возвращает периметр соответсвующего треугольника'''
-    return a + b + c 
+
+def area(a, h):
+    """Принимает длину стороны и высоты треугольника, возвращает площадь соответствующего треугольника"""
+    return a * h / 2
+
+
+def perimeter(a, b, c):
+    """Принимает длины сторон треугольника, возвращает периметр соответствующего треугольника"""
+    return a + b + c
+
+
+class TriangleTestCase(unittest.TestCase):
+    def test_zero_side_area(self):
+        res = area(0, 4)
+        self.assertEqual(res, 0)
+
+    def test_zero_side_perimeter(self):
+        res = perimeter(0, 4, 6)
+        self.assertEqual(res, 10)
+
+    def test_zero_height_area(self):
+        res = area(4, 0)
+        self.assertEqual(res, 0)
+
+    def test_zero_sides_perimeter(self):
+        res = perimeter(0, 0, 0)
+        self.assertEqual(res, 0)
+
+    def test_positive_side_area(self):
+        res = area(3, 4)
+        self.assertEqual(res, 6)
+
+    def test_positive_side_perimeter(self):
+        res = perimeter(3, 4, 5)
+        self.assertEqual(res, 12)
+
+    def test_negative_side_area(self):
+        res = area(-3, 4)
+        self.assertEqual(res, -1)
+
+    def test_negative_height_area(self):
+        res = area(3, -4)
+        self.assertEqual(res, -1)
+
+    def test_negative_sides_perimeter(self):
+        res = perimeter(-1, -2, 3)
+        self.assertEqual(res, -1)
