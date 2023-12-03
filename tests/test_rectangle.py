a345f3614d6ab514df582211e6ea60ac2a51a4d0
dinary-upos <max.orel.site@yandex.kz>
2023-12-03 22:38:48 +0300

    L-06: patch unit tests 2

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -2,21 +2,28 @@ import unittest
 import config
 import rectangle
 
-class TestRectangleMethods(unittest.TestCase):
-	def test_rectangle_perimeter_typeerror(self):
-		self.assertRaises(TypeError, rectangle.perimeter, 'b', 1)
-	def test_rectangle_perimeter_vlaueerror(self):
-		self.assertRaises(ValueError, rectangle.perimeter, -1, 1)
-	def test_rectangle_perimeter_int(self):
-		self.assertEqual(rectangle.perimeter(0, 4), 8)
-	def test_rectangle_perimeter_float(self):
-		self.assertAlmostEqual(rectangle.perimeter(1.5, 2.5), 8, config.kDigitsOfPrecision)
 
-	def test_rectangle_area_typeerror(self):
-		self.assertRaises(TypeError, rectangle.area, 'b', 1)
-	def test_rectangle_area_valueerror(self):
-		self.assertRaises(ValueError, rectangle.area, -1, 1)
-	def test_rectangle_area_int(self):
-		self.assertEqual(rectangle.area(0, 4), 0)
-	def test_rectangle_area_float(self):
-		self.assertAlmostEqual(rectangle.area(1.5, 4.2), 6.3, config.kDigitsOfPrecision)
+class TestRectangleMethods(unittest.TestCase):
+    def test_rectangle_perimeter_typeerror(self):
+        self.assertRaises(TypeError, rectangle.perimeter, 'b', 1)
+
+    def test_rectangle_perimeter_vlaueerror(self):
+        self.assertRaises(ValueError, rectangle.perimeter, -1, 1)
+
+    def test_rectangle_perimeter_int(self):
+        self.assertEqual(rectangle.perimeter(0, 4), 8)
+
+    def test_rectangle_perimeter_float(self):
+        self.assertAlmostEqual(rectangle.perimeter(1.5, 2.5), 8, config.kDigitsOfPrecision)
+
+    def test_rectangle_area_typeerror(self):
+        self.assertRaises(TypeError, rectangle.area, 'b', 1)
+
+    def test_rectangle_area_valueerror(self):
+        self.assertRaises(ValueError, rectangle.area, -1, 1)
+
+    def test_rectangle_area_int(self):
+        self.assertEqual(rectangle.area(0, 4), 0)
+
+    def test_rectangle_area_float(self):
+        self.assertAlmostEqual(rectangle.area(1.5, 4.2), 6.3, config.kDigitsOfPrecision)
--- tests/test_triangle.py
+++ tests/test_triangle.py
@@ -2,21 +2,28 @@ import unittest
 import config
 import triangle
 
-class TestTriangleMethods(unittest.TestCase):
-	def test_triangle_perimeter_typeerror(self):
-		self.assertRaises(TypeError, triangle.perimeter, 'b', 1, 1)
-	def test_triangle_perimeter_valueerror(self):
-		self.assertRaises(ValueError, triangle.perimeter, -1, 1, 2)
-	def test_triangle_perimeter_int(self):
-		self.assertEqual(triangle.perimeter(3, 4, 5), 12)
-	def test_triangle_perimeter_float(self):
-		self.assertAlmostEqual(triangle.perimeter(1.5, 2.5, 3.5), 7.5, config.kDigitsOfPrecision)
 
-	def test_triangle_area_typeerror(self):
-		self.assertRaises(TypeError, triangle.area, 'b', 1)
-	def test_triangle_area_valueerror(self):
-		self.assertRaises(ValueError, triangle.area, -1, 1)
-	def test_triangle_area_int(self):
-		self.assertEqual(triangle.area(6, 3), 9)
-	def test_triangle_area_float(self):
-		self.assertAlmostEqual(triangle.area(1.5, 2.2), 1.65, config.kDigitsOfPrecision)
+class TestTriangleMethods(unittest.TestCase):
+    def test_triangle_perimeter_typeerror(self):
+        self.assertRaises(TypeError, triangle.perimeter, 'b', 1, 1)
+
+    def test_triangle_perimeter_valueerror(self):
+        self.assertRaises(ValueError, triangle.perimeter, -1, 1, 2)
+
+    def test_triangle_perimeter_int(self):
+        self.assertEqual(triangle.perimeter(3, 4, 5), 12)
+
+    def test_triangle_perimeter_float(self):
+        self.assertAlmostEqual(triangle.perimeter(1.5, 2.5, 3.5), 7.5, config.kDigitsOfPrecision)
+
+    def test_triangle_area_typeerror(self):
+        self.assertRaises(TypeError, triangle.area, 'b', 1)
+
+    def test_triangle_area_valueerror(self):
+        self.assertRaises(ValueError, triangle.area, -1, 1)
+
+    def test_triangle_area_int(self):
+        self.assertEqual(triangle.area(6, 3), 9)
+
+    def test_triangle_area_float(self):
+        self.assertAlmostEqual(triangle.area(1.5, 2.2), 1.65, config.kDigitsOfPrecision)
