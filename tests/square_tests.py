b912f24781393dff2d55fa763a25e37c8300bec3
Simon Udaltsov <simonudaltsov@gmail.com>
2023-12-06 00:08:34 +0300

    Revert "rm failure tests"
    This reverts commit 360d4dbe0aeb0c635631a3059175980ccf58d394.

--- tests/square_tests.py
+++ tests/square_tests.py
@@ -2,41 +2,41 @@ import unittest
 import square
 
 class SquareAreaTestCase(unittest.TestCase):
-    # def test_zero_side(self):
-    #     self.assertRaises(ValueError, square.area, 0)
+    def test_zero_side(self):
+        self.assertRaises(ValueError, square.area, 0)
 
     def test_simple(self):
         res = square.area(2)
         self.assertEqual(res, 4)
     
-    # def test_huge(self):
-    #     result = square.area(9999999)
-    #     expected_result = 99999980000001
-    #     self.assertEqual(expected_result, result)
+    def test_huge(self):
+        result = square.area(9999999)
+        expected_result = 99999980000001
+        self.assertEqual(expected_result, result)
     
-    # def test_neg_side(self):
-    #     self.assertRaises(ValueError, square.area, -2)
+    def test_neg_side(self):
+        self.assertRaises(ValueError, square.area, -2)
     
-    # def test_invalid_type(self):
-    #     self.assertRaises(TypeError, square.area, "100")
+    def test_invalid_type(self):
+        self.assertRaises(TypeError, square.area, "100")
 
 class SquarePerTestCase(unittest.TestCase):
-    # def test_zero_side(self):
-    #     self.assertRaises(ValueError, square.perimeter, 0)
+    def test_zero_side(self):
+        self.assertRaises(ValueError, square.perimeter, 0)
     
     def test_simple(self):
         res = square.perimeter(3)
         self.assertEqual(res, 12)
     
-    # def test_huge(self):
-    #     result = square.perimeter(9999999)
-    #     expected_result = 39999996
-    #     self.assertEqual(expected_result, result)
+    def test_huge(self):
+        result = square.perimeter(9999999)
+        expected_result = 39999996
+        self.assertEqual(expected_result, result)
     
-    # def test_neg_side(self):
-    #      self.assertRaises(ValueError, square.perimeter, -2)
+    def test_neg_side(self):
+         self.assertRaises(ValueError, square.perimeter, -2)
     
-    # def test_invalid_type(self):
-    #     self.assertRaises(TypeError, square.area, "100")
+    def test_invalid_type(self):
+        self.assertRaises(TypeError, square.area, "100")
     
     
--- tests/triangle_tests.py
+++ tests/triangle_tests.py
@@ -2,45 +2,45 @@ import unittest
 import triangle
 
 class TriangleAreaTestCase(unittest.TestCase):
-    # def test_zero_side(self):
-    #     self.assertRaises(ValueError, triangle.area, 0, 2)
+    def test_zero_side(self):
+        self.assertRaises(ValueError, triangle.area, 0, 2)
     
-    # def test_zero_height(self):
-    #     self.assertRaises(ValueError, triangle.area, 2, 0)
+    def test_zero_height(self):
+        self.assertRaises(ValueError, triangle.area, 2, 0)
     
     def test_simple(self):
         res = triangle.area(2, 4)
         self.assertEqual(res, 4)
     
-    # def test_huge(self):
-    #     res = triangle.area(9999999, 9999888)
-    #     exp_res = 49999435000056.0
-    #     self.assertEqual(exp_res, res)
+    def test_huge(self):
+        res = triangle.area(9999999, 9999888)
+        exp_res = 49999435000056.0
+        self.assertEqual(exp_res, res)
     
-    # def test_neg_side(self):
-    #     self.assertRaises(ValueError, triangle.area, -2, 2)
+    def test_neg_side(self):
+        self.assertRaises(ValueError, triangle.area, -2, 2)
     
-    # def test_neg_height(self):
-    #     self.assertRaises(ValueError, triangle.area, 2, -2)
+    def test_neg_height(self):
+        self.assertRaises(ValueError, triangle.area, 2, -2)
     
-    # def test_invalid_type(self):
-    #     self.assertRaises(TypeError, triangle.area, "100", 3)
+    def test_invalid_type(self):
+        self.assertRaises(TypeError, triangle.area, "100", 3)
     
 class TrianglePerTestCase(unittest.TestCase):
-    # def test_zero_side(self):
-    #     self.assertRaises(ValueError, triangle.perimeter, 0, 2, 3)
+    def test_zero_side(self):
+        self.assertRaises(ValueError, triangle.perimeter, 0, 2, 3)
     
     def test_simple(self):
         res = triangle.perimeter(2, 4, 3)
         self.assertEqual(res, 9)
     
-    # def test_huge(self):
-    #     res = triangle.perimeter(9999999, 9999888, 9999777)
-    #     exp_res = 29999664
-    #     self.assertEqual(exp_res, res)
+    def test_huge(self):
+        res = triangle.perimeter(9999999, 9999888, 9999777)
+        exp_res = 29999664
+        self.assertEqual(exp_res, res)
     
-    # def test_neg_side(self):
-    #     self.assertRaises(ValueError, triangle.perimeter, -2, 2, 3)
+    def test_neg_side(self):
+        self.assertRaises(ValueError, triangle.perimeter, -2, 2, 3)
     
-    # def test_invalid_type(self):
-    #     self.assertRaises(TypeError, triangle.perimeter, "100", 3, 4)
+    def test_invalid_type(self):
+        self.assertRaises(TypeError, triangle.perimeter, "100", 3, 4)
